09aa7d2ef89792780e8eb990ef20ba08370a4d6a
BarbaraFSilva7 <barbara.fariaspessoal@gmail.com>
2024-10-19 16:04:56 -0400

    Configuração do banco de dados e da tabela

---
+++ HealthMate_PlanoAlimentar/Migrations/20241019195416_M01-AddTableDieta.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HealthMate_PlanoAlimentar.Migrations
+{
+    /// <inheritdoc />
+    public partial class M01AddTableDieta : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PlanoAlimentar",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Idade = table.Column<int>(type: "int", nullable: false),
+                    Sexo = table.Column<string>(type: "nvarchar(1)", nullable: false),
+                    Altura = table.Column<float>(type: "real", nullable: false),
+                    Peso = table.Column<float>(type: "real", nullable: false),
+                    Alergia = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Restricao = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CondSaude = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UsoMedicamento = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Objetivo = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PlanoAlimentar", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PlanoAlimentar");
+        }
+    }
+}
---
+++ HealthMate_PlanoAlimentar/Models/Dieta.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HealthMate_PlanoAlimentar.Models
+{
+    [Table("PlanoAlimentar")]
+    public class Dieta
+    {
+        [Key]   
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Campo Obrigatório!")]
+
+        public string Nome { get; set; }
+
+        [Required(ErrorMessage = "Campo Obrigatório!")]
+        public int Idade { get; set; }
+
+        [Required(ErrorMessage = "Campo Obrigatório!")]
+        public char Sexo { get; set; }
+
+        [Required(ErrorMessage = "Campo Obrigatório!")]
+        public float Altura { get; set; }
+
+        [Required(ErrorMessage = "Campo Obrigatório!")]
+        public float Peso { get; set; }
+
+        [Required(ErrorMessage = "Campo Obrigatório!")]
+        public string Alergia { get; set; }
+
+        [Required(ErrorMessage = "Campo Obrigatório!")]
+        public string Restricao { get; set; }
+
+        [Required(ErrorMessage = "Campo Obrigatório!")]
+        public string CondSaude { get; set; }
+
+        [Required(ErrorMessage = "Campo Obrigatório!")]
+        public string UsoMedicamento { get; set; }
+
+        [Required(ErrorMessage = "Campo Obrigatório!")]
+        public string Objetivo { get; set; }
+    }
+}
